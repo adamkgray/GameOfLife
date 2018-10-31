2937532ad760398a92ab783deb20ddd092bcfabd
Adam Gray <kruschka.kruschka@gmail.com>
2018-10-30 21:13:15 -0400

    refactor

--- main.c
+++ main.c
@@ -38,19 +38,17 @@ int main(void) {
 }
 
 int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
+    /* keep track of how many steps have been taken so far */
+    static int16_t steps = 0;
+    /* The sum of the universe. A sum of 0 (i.e. no active cells) will cause the program to end */
+    int32_t sum = 0;
+    /* Array to store the values of a cells neighbors */
     int8_t neighbors[8];
 
-    /*
-     * neighbors
-     *
-     * 0  1  2
-     * 3  X  4
-     * 5  6  7
-     *
-     */
-
-    /* Calculate next iteration into the parallel universe */
     for (int64_t i = 0; i < (n * n); ++i) {
+        /* increment sum */
+        sum += universe[i];
+
         /* Add universe cell to screen */
         mvaddch(i / n, 2 * (i % n), (universe[i] ? ACS_CKBOARD : ' '));
 
@@ -102,6 +100,8 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
         }
     }
 
+    mvprintw(n + 1, 0, "steps: %d", steps++);
+
     /* Update screen */
     refresh();
 
@@ -113,5 +113,6 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
     /* Wait one second */
     sleep(1);
 
-    return 1;
+    /* If sum is 0, program will exit */
+    return sum;
 }
