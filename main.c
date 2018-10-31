9695b87a7f0dc1942db7383601d13c707315a5de
Adam Gray <kruschka.kruschka@gmail.com>
2018-10-31 12:05:49 -0400

    fix int overflow

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <ncurses.h>
 
-int8_t advance(int8_t * universe, int8_t * parallel_universe, int16_t n);
+int32_t advance(int8_t * universe, int8_t * parallel_universe, int16_t n);
 
 int main(int argc, char ** argv) {
     /* start curses */
@@ -26,12 +26,13 @@ int main(int argc, char ** argv) {
     }
 
     /* the universe is a square, there is also a parallel universe */
-    int8_t universe[n * n], parallel_universe[n * n];
+    int8_t * universe = (int8_t *)malloc((n * n) * sizeof(int8_t));
+    int8_t * parallel_universe = (int8_t *)malloc((n * n) * sizeof(int8_t));
 
     /* fill universe with 1s and 0s */
     srand(time(NULL));
-    for (int64_t i = 0; i < (n * n); ++i) {
-        universe[i] = (rand() & 1);
+    for (int32_t i = 0; i < (n * n); ++i) {
+        universe[i] = rand() & 1;
     }
 
     /* the game of life */
@@ -43,16 +44,15 @@ int main(int argc, char ** argv) {
     return 0;
 }
 
-int8_t advance(int8_t * universe, int8_t * parallel_universe, int16_t n) {
+int32_t advance(int8_t * universe, int8_t * parallel_universe, int16_t n) {
     /* keep track of how many steps have been taken so far */
     static int16_t steps = 0;
 
-    /* The sum of the universe. A sum of 0 (i.e. no active cells) will cause the program to end */
-    int32_t sum = 0;
+    int32_t active = 0;
 
-    for (int64_t i = 0; i < (n * n); ++i) {
-        /* increment sum */
-        sum += universe[i];
+    for (int32_t i = 0; i < (n * n); ++i) {
+        /* increment active */
+        active += universe[i];
 
         /* Add universe cell to screen */
         mvaddch(i / n, 2 * (i % n), (universe[i] ? ACS_CKBOARD : ' '));
@@ -96,23 +96,24 @@ int8_t advance(int8_t * universe, int8_t * parallel_universe, int16_t n) {
                 }
                 break;
             default:
+                return 0;
                 break;
         }
     }
 
-    mvprintw(n + 1, 0, "steps: %d", steps++);
+    mvprintw(n + 1, 0, "steps: %d | active: %.5ld", steps++, active);
 
     /* Update screen */
     refresh();
 
     /* Copy parellel universe into the universe */
-    for (int64_t j = 0; j < (n * n); ++j) {
-        universe[j] = parallel_universe[j];
+    for (int32_t i = 0; i < (n * n); ++i) {
+        universe[i] = parallel_universe[i];
     }
 
     /* Wait one second */
     sleep(1);
 
-    /* If sum is 0, program will exit */
-    return sum;
+    /* If active is 0, program will exit */
+    return active;
 }
