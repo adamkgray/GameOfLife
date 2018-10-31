30f32bfeb46c50cd6116b66f343312367348463e
agray <agray@pch.com>
2018-10-31 10:00:00 -0400

    optimize

--- main.c
+++ main.c
@@ -45,10 +45,9 @@ int main(int argc, char ** argv) {
 int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
     /* keep track of how many steps have been taken so far */
     static int16_t steps = 0;
+
     /* The sum of the universe. A sum of 0 (i.e. no active cells) will cause the program to end */
     int32_t sum = 0;
-    /* Array to store the values of a cells neighbors */
-    int8_t neighbors[8];
 
     for (int64_t i = 0; i < (n * n); ++i) {
         /* increment sum */
@@ -57,47 +56,62 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
         /* Add universe cell to screen */
         mvaddch(i / n, 2 * (i % n), (universe[i] ? ACS_CKBOARD : ' '));
 
-        /* See how the neighbors are */
-        neighbors[0] = (i < n) ? 0 : (i % n == 0) ? 0 : universe[i - n - 1];
-        neighbors[1] = (i < n) ? 0 : universe[i - n];
-        neighbors[2] = (i < n) ? 0 : ((i + 1) % n == 0) ? 0 : universe[i - n + 1];
-
-        neighbors[3] = (i % n == 0) ? 0 : universe[i - 1];
-        neighbors[4] = ((i + 1) % n == 0) ? 0 : universe[i + 1];
-
-        neighbors[5] = (i >= ((n * n) - n)) ? 0 : (i % n == 0) ? 0 : universe[i + n - 1];
-        neighbors[6] = (i >= ((n * n) - n)) ? 0 : universe[i + n];
-        neighbors[7] = (i >= ((n * n) - n)) ? 0 : ((i + 1) % n == 0) ? 0 : universe[i + n + 1];
-
         /* Count active neighbors */
         int8_t active_neighbors = 0;
         for (int8_t j = 0; j < 8; ++j) {
-            active_neighbors += neighbors[j];
+            switch (j) {
+                case 0:
+                    active_neighbors += (i < n) ? 0 : (i % n == 0) ? 0 : universe[i - n - 1];
+                    break;
+                case 1:
+                    active_neighbors += (i < n) ? 0 : universe[i - n];
+                    break;
+                case 2:
+                    active_neighbors += (i < n) ? 0 : ((i + 1) % n == 0) ? 0 : universe[i - n + 1];
+                    break;
+                case 3:
+                    active_neighbors += (i % n == 0) ? 0 : universe[i - 1];
+                    break;
+                case 4:
+                    active_neighbors += ((i + 1) % n == 0) ? 0 : universe[i + 1];
+                    break;
+                case 5:
+                    active_neighbors += (i >= ((n * n) - n)) ? 0 : (i % n == 0) ? 0 : universe[i + n - 1];
+                    break;
+                case 6:
+                    active_neighbors += (i >= ((n * n) - n)) ? 0 : universe[i + n];
+                    break;
+                case 7:
+                    active_neighbors += (i >= ((n * n) - n)) ? 0 : ((i + 1) % n == 0) ? 0 : universe[i + n + 1];
+                    break;
+                default:
+                    break;
+            }
         }
 
         /* Calculate i in parallel_universe*/
         switch (universe[i]) {
-            case 1:
+            case 1:                                 /* active cell */
                 switch (active_neighbors) {
                     case 0:
                     case 1:
-                        parallel_universe[i] = 0;
+                        parallel_universe[i] = 0;   /* too few neigbors */
                         break;
                     case 2:
                     case 3:
-                        parallel_universe[i] = 1;
+                        parallel_universe[i] = 1;   /* right amount of neighbors */
                         break;
                     default:
-                        parallel_universe[i] = 0;
+                        parallel_universe[i] = 0;   /* too many neighbors */
                         break;
                 }
                 break;
-            case 0:
+            case 0:                                 /* inactive cell */
                 switch (active_neighbors) {
-                    case 3:
+                    case 3:                         /* right amount of neighbors */
                         parallel_universe[i] = 1;
                         break;
-                    default:
+                    default:                        /* too few or too many neighbors */
                         parallel_universe[i] = 0;
                         break;
                 }
