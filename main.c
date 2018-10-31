a3ede1a5a90ed41885e9cb581d997339f6fa2d7f
Adam Gray <kruschka.kruschka@gmail.com>
2018-10-30 21:27:17 -0400

    get n from args

--- main.c
+++ main.c
@@ -9,9 +9,14 @@
 
 int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n);
 
-int main(void) {
+int main(int8_t argc, char ** argv) {
     /* Set n */
-    int32_t n = 10;
+    int32_t n;
+    if (argc < 2) {
+        n = 10;
+    } else {
+        n = (int32_t)atoi(argv[1]);
+    }
 
     /* the universe is a square, there is also a parallel universe */
     int64_t universe[n * n], parallel_universe[n * n];
@@ -19,7 +24,7 @@ int main(void) {
     /* fill universe with 1s and 0s */
     srand(time(NULL));
     for (int64_t i = 0; i < (n * n); ++i) {
-        universe[i] = (rand() & 1) && (rand() & 1) && (rand() & 1) && (rand() & 1);
+        universe[i] = (rand() & 1);
     }
 
     /* start curses */
@@ -53,14 +58,14 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
         mvaddch(i / n, 2 * (i % n), (universe[i] ? ACS_CKBOARD : ' '));
 
         /* See how the neighbors are */
-        neighbors[0] = (i <= n) ? 0 : ((i % 10 == 0) ? 0 : universe[i - 11]);
-        neighbors[1] = (i <= n) ? 0 : universe[i - 10];
-        neighbors[2] = (i <= n) ? 0 : ((i % 9 == 0) ? 0 : universe[i - 9]);
-        neighbors[3] = (i % 10 == 0) ? 0 : universe[i - 1];
-        neighbors[4] = (i % 9 == 0) ? 0 : universe[i + 1];
-        neighbors[5] = (i >= ((n * n) - n)) ? 0 : ((i % 10 == 0) ? 0 : universe[i + 9]);
-        neighbors[6] = (i >= ((n * n) - n)) ? 0 : universe[i + 10];
-        neighbors[7] = (i >= ((n * n) - n)) ? 0 : ((i % 9 == 0) ? 0 : universe[i + 11]);
+        neighbors[0] = (i <= n) ? 0 : ((i % n == 0) ? 0 : universe[i - (n + 1)]);
+        neighbors[1] = (i <= n) ? 0 : universe[i - n];
+        neighbors[2] = (i <= n) ? 0 : ((i % (n - 1) == 0) ? 0 : universe[i - (n - 1)]);
+        neighbors[3] = (i % n == 0) ? 0 : universe[i - 1];
+        neighbors[4] = (i % (n - 1) == 0) ? 0 : universe[i + 1];
+        neighbors[5] = (i >= ((n * n) - n)) ? 0 : ((i % n == 0) ? 0 : universe[i + (n - 1)]);
+        neighbors[6] = (i >= ((n * n) - n)) ? 0 : universe[i + n];
+        neighbors[7] = (i >= ((n * n) - n)) ? 0 : ((i % (n - 1) == 0) ? 0 : universe[i + (n + 1)]);
 
         /* Count active neighbors */
         int8_t active_neighbors = 0;
@@ -70,7 +75,7 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
 
         /* Calculate i in parallel_universe*/
         switch (universe[i]) {
-            case 0:
+            case 1:
                 switch (active_neighbors) {
                     case 0:
                     case 1:
@@ -85,7 +90,7 @@ int8_t advance(int64_t * universe, int64_t * parallel_universe, int32_t n) {
                         break;
                 }
                 break;
-            case 1:
+            case 0:
                 switch (active_neighbors) {
                     case 3:
                         parallel_universe[i] = 1;
